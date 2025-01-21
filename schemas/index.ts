1ed38081fe23dc501d6408744a6766647266388f
Aleksa <aleksa3b@gmail.com>
2025-01-21 23:29:49 +0100

    post zod schema

--- schemas/index.ts
+++ schemas/index.ts
@@ -7,3 +7,16 @@ export const SignUpSchema = z.object({
     .email({ message: "Please enter a valid email address!" })
     .trim(),
 });
+
+export const PostSchema = z.object({
+  name: z
+    .string()
+    .min(1, { message: "Post name is required!" })
+    .max(100, { message: "Post name cannot exceed 100 characters!" }),
+  image: z
+    .any()
+    .refine((file) => file instanceof File || typeof file === "string", {
+      message: "Image must be a valid file or a valid URL!",
+    }),
+  description: z.string().min(1, { message: "Description is required!" }),
+});
