c38cf3d8894af0c91b2cc3cfbe9cd0b0c69116eb
Aleksa <aleksa3b@gmail.com>
2025-01-27 22:59:55 +0100

    form for adding posts

--- app/admin/components/Posts.tsx
+++ app/admin/components/Posts.tsx
@@ -1,28 +1,33 @@
 import React from "react";
 
-const Ads = () => {
+const Posts = () => {
   return (
-    <article className="mt-28">
-      <h3 className="text-2xl">Add an ad</h3>
+    <article className="my-48">
+      <h3 className="text-2xl">Add a post for a restaurant</h3>
       <form className="flex justify-center items-center flex-col mt-5 w-1/2 mx-auto">
         <input
           type="text"
           className="bg-primary text-body outline-none placeholder-body text-md tracking-wide w-full font-outfit px-3 py-2 my-2 border-4 border-transparent rounded-md focus:border-secondary focus:border-4"
           placeholder="Name of a dish..."
         />
-        <input
-          type="text"
-          className="bg-primary text-body outline-none placeholder-body text-md tracking-wide w-full font-outfit px-3 py-2 my-2 border-4 border-transparent rounded-md focus:border-secondary focus:border-4"
-          placeholder="Discount (add % after number)..."
+        <textarea
+          className="max-h-[500px] bg-primary text-body outline-none placeholder-body text-md tracking-wide w-full font-outfit px-3 py-2 my-2 border-4 border-transparent rounded-md focus:border-secondary focus:border-4"
+          placeholder="Description"
         />
         <input
           type="text"
           className="bg-primary text-body outline-none placeholder-body text-md tracking-wide w-full font-outfit px-3 py-2 my-2 border-4 border-transparent rounded-md focus:border-secondary focus:border-4"
-          placeholder="Days active (e.g. 21.1. - 28.1.)..."
+          placeholder="Price (add £ after number)..."
+        />
+        <input
+          type="number"
+          className="bg-primary text-body outline-none placeholder-body text-md tracking-wide w-full font-outfit px-3 py-2 my-2 border-4 border-transparent rounded-md focus:border-secondary focus:border-4"
+          placeholder="Restaurant"
         />
         <input
           type="file"
           name="images"
+          multiple
           accept="image/*"
           className="bg-primary text-body outline-none placeholder-body text-md tracking-wide w-full font-outfit px-3 py-2 my-2 border-4 border-transparent rounded-md focus:border-secondary focus:border-4"
         />
@@ -34,4 +39,4 @@ const Ads = () => {
   );
 };
 
-export default Ads;
+export default Posts;
--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 
 import Ads from "./components/Ads";
+import Posts from "./components/Posts";
 
 const AdminPage = () => {
   return (
     <section className="px-10 text-center">
       <h2>AdminPage</h2>
       <Ads />
+      <Posts />
     </section>
   );
 };
