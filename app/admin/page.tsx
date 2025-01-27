b10bd202a769c94b5783f32551e80ca04ff859d0
Aleksa <aleksa3b@gmail.com>
2025-01-27 22:53:18 +0100

    form for adding ads

---
+++ app/admin/page.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+
+import Ads from "./components/Ads";
+
+const AdminPage = () => {
+  return (
+    <section className="px-10 text-center">
+      <h2>AdminPage</h2>
+      <Ads />
+    </section>
+  );
+};
+
+export default AdminPage;
